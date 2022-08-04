c283f62297fe0e4de53ce2392ffd3093e0e1d72c
YeonJun Cho <micayel1219@gmail.com>
2022-08-04 14:45:05 +0900

    db user setting

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -4,7 +4,7 @@ public class App {
     public static final String BASE_PACKAGE_PATH = "com.ll.exam";
     public static final String DB_HOST = "localhost";
     public static final int DB_PORT = 3306;
-    public static final String DB_ID = "lldj";
-    public static final String DB_PASSWORD = "lldj1234";
+    public static final String DB_ID = "likelion";
+    public static final String DB_PASSWORD = "likelion123";
     public static final String DB_NAME = "blog";
 }
