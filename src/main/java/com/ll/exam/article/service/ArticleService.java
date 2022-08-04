4b24fc89e4d0f2d0037775c3b2eaf67ca912392e
YeonJun Cho <micayel1219@gmail.com>
2022-08-04 14:31:44 +0900

    write 구현

--- src/main/java/com/ll/exam/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/article/service/ArticleService.java
@@ -23,4 +23,9 @@ public class ArticleService {
     public long getArticlesCount() {
         return articleRepository.getArticlesCount();
     }
+
+
+    public long write(String title, String body, boolean isBlind) {
+        return articleRepository.write(title, body, isBlind);
+    }
 }
