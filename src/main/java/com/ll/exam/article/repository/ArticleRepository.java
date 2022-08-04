0f4266a1d3d393c06cf1e6ac50c69e0f91ce1329
YeonJun Cho <micayel1219@gmail.com>
2022-08-04 14:40:27 +0900

    이전글 테스트 케이스 추가 및 구현

--- src/main/java/com/ll/exam/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/article/repository/ArticleRepository.java
@@ -74,4 +74,15 @@ public class ArticleRepository {
 
         sql.update();
     }
+
+    public ArticleDto getPrevArticle(long id) {
+        SecSql sql = myMap.genSecSql();
+        sql
+                .append("SELECT *")
+                .append("FROM article")
+                .append("WHERE id < ?", id)
+                .append("ORDER BY id DESC")
+                .append("LIMIT 1");
+        return sql.selectRow(ArticleDto.class);
+    }
 }
