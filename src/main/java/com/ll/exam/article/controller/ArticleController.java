8e9e2820924ae7d9ef29081e65cc984a9f369e87
YeonJun Cho <micayel1219@gmail.com>
2022-08-04 14:53:23 +0900

    게시물 리스트 구현

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -16,7 +16,7 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @GetMapping("/usr/article/list/{boardCode}") // /usr/article/list/free 와 같이 관련된 요청을 처리하는 함수이다.
+    @GetMapping("/usr/article/list") // /usr/article/list/free 와 같이 관련된 요청을 처리하는 함수이다.
     // 아래 showList 는 Get /usr/article/list 으로 요청이 왔을 때 실행 되어야 하는 함수이다.
     public void showList(Rq rq) {
         List<ArticleDto> articleDtos = articleService.getArticles();
@@ -25,25 +25,4 @@ public class ArticleController {
         rq.view("usr/article/list");
     }
 
-    @GetMapping("/usr/article/detail/{boardCode}/{id}")
-    public void showDetail(Rq rq) {
-        rq.println("게시물 상세페이지<br>");
-
-        // long id = rq.getLongPathValueByIndex(1, -1);
-        String boardCode = rq.getParam("boardCode", ""); // 곧 기능 구현
-        long id = rq.getLongParam("id", -1); // 곧 기능 구현
-
-        rq.println("%s 게시판, %d번 게시물".formatted(boardCode, id));
-    }
-
-    @GetMapping("/usr/article/modify/{boardCode}/{id}")
-    public void showModify(Rq rq) {
-        rq.println("게시물 수정페이지<br>");
-
-        // long id = rq.getLongPathValueByIndex(1, -1);
-        String boardCode = rq.getParam("boardCode", ""); // 곧 기능 구현
-        long id = rq.getLongParam("id", -1); // 곧 기능 구현
-
-        rq.println("%s 게시판, %d번 게시물".formatted(boardCode, id));
-    }
 }
